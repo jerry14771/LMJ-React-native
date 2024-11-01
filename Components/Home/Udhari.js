bdd3d708945496a3beab0fcae3d88051e17b32ae
ROHIT RAJ <rj14771@gmail.com>
2024-11-01 23:30:04 +0530

    finalize 1st module

--- Components/Home/Udhari.js
+++ Components/Home/Udhari.js
@@ -1,14 +1,26 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Header from './Header'
+import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import Header from './Header';
+import Calendar from 'date-bengali-revised'
+ 
+
 
 const Udhari = () => {
+
+  let cal = new Calendar()
+
+  console.log(cal.fromGregorian(2024, 10, 30));
+  let bacl = new Calendar(1425, 1, 1)
+  bacl.format()
+//> '১ ১, ১৪২৫'
+console.log(bacl.format('dddd D MMMM, Y [Q]'))
+
   return (
     <View>
-      <Header/>
-      <Text>Udhari</Text>
+      <Header />
+      <Text>hello</Text>
     </View>
-  )
-}
+  );
+};
 
-export default Udhari
+export default Udhari;
